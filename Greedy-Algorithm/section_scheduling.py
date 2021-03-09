ff37f7deb3c1caa4702e433973392720b2dbc05e
keishi25 <txijk1154.asd@gmail.com>
2021-03-09 09:41:25 +0900

    Fix selection geedy algorithm

--- Greedy-Algorithm/section_scheduling.py
+++ Greedy-Algorithm/section_scheduling.py
@@ -2,6 +2,9 @@
 問題リンク
 https://atcoder.jp/contests/keyence2020/tasks/keyence2020_b
 
+解説リンク
+https://nashidos.hatenablog.com/entry/2020/01/19/120045
+
 方針
 リストに始点と終点を格納し、終点でソート
 小さい順に、範囲が重なりがないところをカウントアップしていく
@@ -18,15 +21,14 @@ for i in range(N):
 # 終点でソート
 start_end_list = sorted(start_end_list, key=lambda x: x[1])
 
-ans = 1
-# 重なりがないところを貪欲に探索
-for i in range(len(start_end_list) - 1):
-    start_before = start_end_list[i][0]
-    end_before = start_end_list[i][1]
-    start_after = start_end_list[i + 1][0]
-    end_after = start_end_list[i + 1][1]
+ans = N
 
-    if end_before < start_after:
-        ans += 1
+# 重なりが発生している箇所を間引く
+for i in range(len(start_end_list) - 1):
+
+    if start_end_list[i][1] > start_end_list[i + 1][0]:
+        # 重なりがある場合、間引く処理を行うため、座標の書き換えを行っている
+        start_end_list[i + 1][1] = start_end_list[i][1]
+        ans -= 1
 
 print(ans)
